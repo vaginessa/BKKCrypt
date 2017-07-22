ac4e78fccad97fe826b9e2df3e054a7fa682b921
Tamás <szabto@gmail.com>
2017-07-22 15:20:30 +0200

    C#

---
+++ bkkcrypt.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BKK 
+{
+	public interface ICrypt
+	{
+		string Encrypt(string input);
+	}
+
+	public class BKKCrypt : ICrypt
+	{
+    	public string Encrypt(string input)
+    	{
+      		return input;
+    	}
+  	}
+}
