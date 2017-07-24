b974c70a46c729677d2a366d2fe6142b4fe1c6f7
Dávid Naményi <dnamenyi@gmail.com>
2017-07-24 16:25:37 +0200

    Added type declarations and PHPDoc

--- PHP/bkkcrypt.php
+++ PHP/bkkcrypt.php
@@ -1,3 +1,9 @@
-function BKKCrypt($password) {
+/**
+ * Encrypts and returns the input string using the famous BKK cryptographic method
+ *
+ * @param string $password
+ * @return string
+ */
+function BKKCrypt(string $password): string {
     return $password;
 }
