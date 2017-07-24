d01f73d0beac12da9af8279136c15c0067702a0f
John White <jana_tb@live.com>
2017-07-25 00:49:31 +0200

    Create BkkCrypt.ts

---
+++ TypeScript/BkkCrypt.ts
@@ -0,0 +1,8 @@
+/**
+ * BkkCrypt - Encrypts and returns the input string using the famous BKK cryptographic method.
+ * @param input - String to be safely encrypted by the algorythm.
+ * @return Safely encrypted string output of the program.
+ */
+const BkkCrypt = (input: string) => input;
+
+export { BkkCrypt }
