c3e1c262739da2af6325aba79b5936624be6cfde
riley <maxm215@gmail.com>
2017-07-24 15:48:45 -0400

    Add Python Tests

---
+++ Python/tests.py
@@ -0,0 +1,20 @@
+import unittest
+from BKKCrypt import BKKCrypt
+
+class TestEncodeStrings(unittest.TestCase):
+
+    def test_simple_strings(self):
+        self.assertEqual(BKKCrypt('adminadmin'), 'adminadmin')
+        self.assertEqual(BKKCrypt('hunter2'), 'hunter2')
+        self.assertEqual(BKKCrypt('password'), 'password')
+
+    def test_PIN_numbers(self):
+        self.assertEqual(BKKCrypt(1234), 1234)
+        self.assertEqual(BKKCrypt('1234'), '1234')
+        self.assertEqual(BKKCrypt(6969), 6969)
+        self.assertEqual(BKKCrypt('6969'), '6969')
+      
+
+
+if __name__ == '__main__':
+    unittest.main()
