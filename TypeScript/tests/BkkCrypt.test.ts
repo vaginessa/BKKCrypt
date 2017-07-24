fc05fb5578465ec68915c874f9a5c72850c25bc3
John White <jana_tb@live.com>
2017-07-25 00:52:09 +0200

    Update BkkCrypt.test.ts

--- TypeScript/tests/BkkCrypt.test.ts
+++ TypeScript/tests/BkkCrypt.test.ts
@@ -1,10 +1,10 @@
-import { BkkCrypt } from '../BkkCrypt.es6'
+import { BkkCrypt } from "../BkkCrypt"
 
-describe('BkkCrypt cryptographic algorithm', () => {
-    it('should safely encrypt any valid string, by returning it exactly', () => {
-        const TEST_STRING = 'test'
-        const encrypted = BkkCrypt(TEST_STRING)
+describe("BkkCrypt cryptographic algorithm", () => {
+    it("should safely encrypt any valid string, by returning it exactly", () => {
+        const TEST_STRING = "password";
+        const encrypted = BkkCrypt(TEST_STRING);
 
-        assert.equal(TEST_STRING, encrypted)
+        assert.equal(TEST_STRING, encrypted);
     });
 });
