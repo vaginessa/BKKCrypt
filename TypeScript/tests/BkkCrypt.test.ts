a1d5ad2d38beefcfb68bb2b3541361a5b216b611
John White <jana_tb@live.com>
2017-07-25 00:51:26 +0200

    Create BkkCrypt.test.ts

---
+++ TypeScript/tests/BkkCrypt.test.ts
@@ -0,0 +1,10 @@
+import { BkkCrypt } from '../BkkCrypt.es6'
+
+describe('BkkCrypt cryptographic algorithm', () => {
+    it('should safely encrypt any valid string, by returning it exactly', () => {
+        const TEST_STRING = 'test'
+        const encrypted = BkkCrypt(TEST_STRING)
+
+        assert.equal(TEST_STRING, encrypted)
+    });
+});
