e2d4f9502730ca80bbcba267688a52fdf1b4868a
PumpkinSeed <fabian.ferenc@webprogblog.com>
2017-07-24 16:46:28 +0100

    Fix conflict in Go solution test

--- Go/bkkcrypt_test.go
+++ Go/bkkcrypt_test.go
@@ -2,11 +2,27 @@ package bkkcrypt
 
 import "testing"
 
-func TestSum(t *testing.T) {
-	password := "verysecure"
-	data := []byte(password)
-	encrypted := Sum(data)
-	if string(encrypted) != password {
-		t.Errorf("Encrypted should be %s, instead of %s", password, encrypted)
+type testCase struct {
+	input    string
+	expected string
+}
+
+var testCases = []testCase{
+	{"password", "password"},
+	{"asdf", "asdf"},
+}
+
+func TestBKKCrypt(t *testing.T) {
+	for _, test := range testCases {
+		act := Sum([]byte(test.input))
+		if string(act) != test.expected {
+			t.Fatalf("Actual '%s' did not equal expected '%s'", act, test.expected)
+		}
+	}
+}
+
+func BenchmarkBKKCrypt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum([]byte("password"))
 	}
 }
