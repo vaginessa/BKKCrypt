9825edc71e018046223365eea73543f9f8a43cf9
daniel.eszeki <daniel.eszeki@tandofer.hu>
2017-07-25 16:12:25 +0200

    Add VB.NET implementation with unit test

---
+++ VB.NET/BKKCrypt.vb
@@ -0,0 +1,13 @@
+﻿
+Public Interface ICrypt
+    Function Encrypt(ByVal input As String) As String
+End Interface
+
+Public Class BKKCrypt
+    Implements ICrypt
+
+    Function Encrypt(ByVal input As String) As String Implements ICrypt.Encrypt
+        Return input
+    End Function
+End Class
+
---
+++ VB.NET/BKKCryptUnitTest.vb
@@ -0,0 +1,10 @@
+﻿<TestClass()> Public Class BKKCryptUnitTest
+
+    <TestMethod()> Public Sub TestEncrypt()
+        Dim superPassword = "mySuperPassword"
+        Dim crypt = New BKKCrypt.BKKCrypt()
+
+        Assert.AreEqual(superPassword, crypt.Encrypt(superPassword))
+    End Sub
+
+End Class
