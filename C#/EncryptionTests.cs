b3b1e6b4e6b890d634177d2fd999b4ee1f738666
Veninger <mordana666@gmail.com>
2017-07-26 20:08:03 +0200

    Added some factories with unit tests and some typo fix

--- C#/EncryptionTests.cs
+++ C#/EncryptionTests.cs
@@ -9,11 +9,11 @@ namespace BKK
         /// Encryptor test
         /// </summary>
         [TestMethod]
-        public void EncyrptionTest()
+        public void EncryptionTest()
         {
             //Arrange
             const string password = "mySuperSecurePassword";
-            BKKCrypt bkkCrypt = new BKKCrypt();
+            BKKCrypt bkkCrypt = (BKKCrypt)new BkkCryptFactory().CreateCrypt();
 
             //Act
             string encryptedPassword = bkkCrypt.Encrypt(password);
@@ -52,6 +52,42 @@ namespace BKK
             //Arrange
             const string password = "mySuperSecurePassword";
             StrategyContext context = new StrategyContext(new BKKCryptStrategy());
+
+            //Act
+            string encryptedPassword = context.Crypt(password);
+
+            //Assert
+            Assert.IsNotNull(password);
+            Assert.IsNotNull(encryptedPassword);
+            Assert.AreEqual(encryptedPassword, password);
+            Assert.AreNotEqual(encryptedPassword,
+                "394257263CF893FE61D72E8E3B1E625F297CDACD5FD2774C0239C4A8A1D4D783F7363B4A20A3848BFD7A7A6833EB858A631CECBE2A297F3AB868111028E4FEF6"); //sha512
+        }
+
+        [TestMethod]
+        public void FactoryEncryptionTest()
+        {
+            //Arrange
+            const string password = "mySuperSecurePassword";
+            BKKCrypt bkkCrypt = (BKKCrypt)new BkkCryptFactory().CreateCrypt();
+
+            //Act
+            string encryptedPassword = bkkCrypt.Encrypt(password);
+
+            //Assert
+            Assert.IsNotNull(password);
+            Assert.IsNotNull(encryptedPassword);
+            Assert.AreEqual(encryptedPassword, password);
+            Assert.AreNotEqual(encryptedPassword,
+                "394257263CF893FE61D72E8E3B1E625F297CDACD5FD2774C0239C4A8A1D4D783F7363B4A20A3848BFD7A7A6833EB858A631CECBE2A297F3AB868111028E4FEF6"); //sha512
+        }
+
+        [TestMethod]
+        public void FactoryCryptStrategyTest()
+        {
+            //Arrange
+            const string password = "mySuperSecurePassword";
+            StrategyContext context = new ContextFactory().CreateContext(new BKKStrategyFactory().CreateStrategy());
 
             //Act
             string encryptedPassword = context.Crypt(password);
